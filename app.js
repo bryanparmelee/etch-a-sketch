d75efd887e509b66fd88ba79f48e0c1e1cbdb585
bryanparmelee <bryan.parmelee@gmail.com>
2022-04-22 17:11:33 -0400

    Fix gridMaker fucntion
    gridmaker Function wasn't creating grids with row and columns equal
    to a given value. This is fixed now.

--- app.js
+++ app.js
@@ -1,34 +1,24 @@
 const grid = document.querySelector('.grid-container');
-const btn = document.querySelector('#btn');
+const btn = document.querySelector('.btn');
 
 const gridMaker = (num) => {
-    let html = '';
-    const cell = '<div class="cell"></div>';
-    html += cell.repeat(num * num);
-    grid.innerHTML = html;
-}
-
-
-
-gridMaker(16);
-
-
-const cells = document.querySelectorAll('.cell');
-
-cells.forEach((cell) => {
-    cell.addEventListener('mouseover', () => {
-    cell.classList.add('hover');
-    })
-    cell.addEventListener('mousedown', () => {
-        cell.classList.remove('hover');
+    grid.setAttribute('style', `grid: repeat(${num}, 1fr) / repeat(${num}, 1fr);`)
+    for (let i = 0; i < num * num; i++) {
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        grid.appendChild(cell);
     }
-    )
-});
-
-const erase = () => {
-    cells.forEach((cell) => {
-        cell.classList.remove('hover');
-    })
 }
 
-btn.addEventListener('click', erase());
+
+
+gridMaker(60);
+// btn.addEventListener('click', clear());
+
+
+
+
+
+
+
+
