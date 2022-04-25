ca4b28294403da8058beea7f4bca428648decef9
bryanparmelee <bryan.parmelee@gmail.com>
2022-04-24 21:46:02 -0400

    Create random rga code generator
    Created a random function that returns a random rga value.

--- app.js
+++ app.js
@@ -32,8 +32,20 @@ slider.addEventListener('change', (e) => {
     erase();
 })
 
+const random = () => {
+   let r = Math.floor(Math.random() * 255);
+   let g = Math.floor(Math.random() * 255);
+   let b = Math.floor(Math.random() * 255);
+   return "rgb(" + r + ", " + g + ", " + b + ");";
+}
+
+console.log(random());
+
+
 
 gridMaker(slider.value);
+
+
 
 
 
